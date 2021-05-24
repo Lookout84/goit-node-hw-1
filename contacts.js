46d1c1a0e378286d4a9edb4d6a2b6ac3761c8eed
Oleksandr Ivashchuk <thelookout84@gmail.com>
2021-05-24 14:18:19 +0300

    uodate

--- contacts.js
+++ contacts.js
@@ -1,24 +1,39 @@
 const fs = require("fs").promises;
 const path = require("path");
+const { v4: uuid } = require('uuid')
 
-const contactsPath = path.join("./db/contacts.json");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 // TODO: задокументировать каждую функцию
 function listContacts() {
-  fs.readFile(contactsPath, (err, data) => {
-    if (err) {
-      console.error(err.message);
-    }
-    return;
-  });
+  const data = fs.readFile(contactsPath, 'utf8')
+  return JSON.parse(data)
+
+  // fs.readFile(contactsPath, (err, data) => {
+  //   if (err) {
+  //     console.error(err.message);
+  //   }
+  //   return;
+  // };
   // ...твой код
 }
 
 function getContactById(contactId) {
+  const data = listContacts()
+  const [result] = data.filter((contact) => contact.contactId === contactId)
+  return result
   // ...твой код
 }
 
 function removeContact(contactId) {
+  const data = listContacts()
+  const index = data.findIndex((contact) => contact.contactId === contactId)
+  if (index !== -1) {
+    const result = data.splice(index, 1)
+    fs.writeFile(contactsPath, JSON.stringify(data))
+    return result
+  }
+  return null
   // ...твой код
 }
 
